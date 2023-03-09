dbd45699229efe286c752890b64cdf2f841e40a4
Vafin Stanislav <stasvafin@gmail.com>
2023-03-10 02:58:40 +0700

    empty spring application created

---
+++ src/main/java/org/zeveon/drones/DronesApplication.java
@@ -0,0 +1,15 @@
+package org.zeveon.drones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Stanislav Vafin
+ */
+@SpringBootApplication
+public class DronesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronesApplication.class);
+    }
+}
