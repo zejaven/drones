33572924a19667c5f5736d88e44adf5704153bda
Vafin Stanislav <stasvafin@gmail.com>
2023-03-17 08:12:48 +0700

    Base64ToMultipartFileDeserializerTest implemented

---
+++ src/main/test/org/zeveon/drones/component/Base64ToMultipartFileDeserializerTest.java
@@ -0,0 +1,46 @@
+package org.zeveon.drones.component;
+
+import com.fasterxml.jackson.core.JsonParser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Stanislav Vafin
+ */
+class Base64ToMultipartFileDeserializerTest {
+
+    private Base64ToMultipartFileDeserializer deserializer;
+
+    @BeforeEach
+    void setUp() {
+        deserializer = new Base64ToMultipartFileDeserializer();
+    }
+
+    @Test
+    void testDeserialize() throws IOException {
+        var fileName = "imageFile.jpg";
+        var contentType = "image/jpeg";
+        var content = "image-content";
+        var base64Content = Base64.getEncoder().encodeToString(content.getBytes(StandardCharsets.UTF_8));
+        var base64String = String.format("%s:%s:%s", fileName, contentType, base64Content);
+
+        var parser = mock(JsonParser.class);
+        when(parser.getText()).thenReturn(base64String);
+
+        var result = deserializer.deserialize(parser, null);
+
+        assertNotNull(result);
+        assertEquals(fileName, result.getOriginalFilename());
+        assertEquals(contentType, result.getContentType());
+        assertEquals(content, new String(result.getBytes(), StandardCharsets.UTF_8));
+    }
+}
